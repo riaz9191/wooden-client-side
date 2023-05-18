f9139121399fab7d45546347f59bc9063f4583b6
riaz9191 <riazahammed9191@gmail.com>
2023-05-18 17:44:30 +0600

    extra two section

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -37,7 +37,7 @@ const SignUp = () => {
           updateProfileData(name,photo)
 
           form.reset();
-          setSuccess("Successfully Created Profile ");
+          setSuccess("Successfully Created Profile");
 
           navigate(from);
           // ...
