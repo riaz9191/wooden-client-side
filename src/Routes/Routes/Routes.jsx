30a2922f4d16fbedad126e3d081f23ea9bbef30c
riaz9191 <riazahammed9191@gmail.com>
2023-05-18 11:01:39 +0600

    initializing setup

---
+++ src/Routes/Routes/Routes.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
+import HomePage from "../../Pages/HomePage/HomePage";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+          element: <HomePage></HomePage>,
+        },
+      ],
+    },
+  ]);
+  
+  export default router;
+  
